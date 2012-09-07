0c71985f28162bf007df399e576d4fd2144a7126
云风 <cloudwu@gmail.com>
2012-09-07 11:19:03 +0800

    service manager

---
+++ service/service_mgr.lua
@@ -0,0 +1,23 @@
+local skynet = require "skynet"
+
+local service = {}
+
+local function query(service_name, ...)
+	local s = service[service_name]
+	if s == nil then
+		s = skynet.newservice(service_name, ...)
+		service[service_name] = s
+	end
+	return s
+end
+
+skynet.start(function()
+	skynet.dispatch("lua", function(session, address, service_name , ...)
+		local handle = query(service_name, ...)
+		skynet.ret(skynet.pack(handle))
+	end)
+	skynet.register(".service")
+	if skynet.getenv "standalone" then
+		skynet.register("SERVICE")
+	end
+end)
