b6f57aee0b208e22b03f5f089b79b95548a8e554
云风 <cloudwu@gmail.com>
2013-12-16 13:54:29 +0800

    define max client for example

--- service/main.lua
+++ service/main.lua
@@ -1,12 +1,14 @@
 local skynet = require "skynet"
 
+local max_client = 64
+
 skynet.start(function()
 	print("Server start")
 	local service = skynet.newservice("service_mgr")
 	skynet.monitor "simplemonitor"
 	local lualog = skynet.newservice("lualog")
 	local console = skynet.newservice("console")
-	local watchdog = skynet.newservice("watchdog","8888 4 0")
+	local watchdog = skynet.newservice("watchdog","8888", max_client, 0)
 	local db = skynet.newservice("simpledb")
 --	skynet.launch("snlua","testgroup")
 
