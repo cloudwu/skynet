aaf8617dd53676ea734a17661885504faabded10
Cloud Wu <cloudwu@gmail.com>
2014-10-28 17:14:30 +0800

    skynet_free(NULL) is ok

--- skynet-src/skynet_socket.c
+++ skynet-src/skynet_socket.c
@@ -61,8 +61,7 @@ forward_message(int type, bool padding, struct socket_message * result) {
 	if (skynet_context_push((uint32_t)result->opaque, &message)) {
 		// todo: report somewhere to close socket
 		// don't call skynet_socket_close here (It will block mainloop)
-		if (sm->buffer)
-			skynet_free(sm->buffer);
+		skynet_free(sm->buffer);
 		skynet_free(sm);
 	}
 }
