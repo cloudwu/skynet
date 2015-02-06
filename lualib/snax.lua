0447f661e0a9fadf655a6a376258527077830011
dpull <beingstudio@gmail.com>
2015-02-06 10:01:19 +0800

    snax 的 G 可配置

--- lualib/snax.lua
+++ lualib/snax.lua
@@ -4,7 +4,9 @@ local snax_interface = require "snax.interface"
 local snax = {}
 local typeclass = {}
 
-local G = { require = function() end }
+local interface_g = skynet.getenv("snax_interface_g")
+local G = interface_g and require (interface_g) or { require = function() end }
+interface_g = nil
 
 skynet.register_protocol {
 	name = "snax",
