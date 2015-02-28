9a6e26db96f9af805fa1f2f76d3fc85ecd76953c
Cloud Wu <cloudwu@gmail.com>
2015-02-28 16:42:47 +0800

    add sharemap , an example of stm

--- lualib/sproto.lua
+++ lualib/sproto.lua
@@ -53,9 +53,9 @@ function sproto:encode(typename, tbl)
 	return core.encode(st, tbl)
 end
 
-function sproto:decode(typename, bin)
+function sproto:decode(typename, ...)
 	local st = querytype(self, typename)
-	return core.decode(st, bin)
+	return core.decode(st, ...)
 end
 
 function sproto:pencode(typename, tbl)
---
+++ test/sharemap.sp
@@ -0,0 +1,5 @@
+.foobar {
+	x 0 : integer
+	y 1 : integer
+	s 2 : string
+}
