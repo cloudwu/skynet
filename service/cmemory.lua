ec1c338167fdc9091827f0061a524e578e3f21ec
snail <okniuys@gmail.com>
2015-12-14 16:33:09 +0800

    improve memory log description
    'total' doesn't account for lua memory.

--- service/cmemory.lua
+++ service/cmemory.lua
@@ -8,7 +8,7 @@ for k,v in pairs(info) do
 	print(string.format(":%08x %gK",k,v/1024))
 end
 
-print("Total memory:", memory.total())
-print("Total block:", memory.block())
+print("Total c memory:", memory.total())
+print("Total c block:", memory.block())
 
 skynet.start(function() skynet.exit() end)
