fd1b48673424b4577a2724e0d199fff0b31841a0
mrChang0 <564415104@qq.com>
2016-11-05 08:13:24 +0800

    update testuart.lua

--- test/testuart.lua
+++ test/testuart.lua
@@ -1,9 +1,12 @@
 local skynet = require "skynet"
 local uart = require "uart"
 
+local fd
 skynet.start(function()
 	local cb = function(id,data)
-		print("recv data:"..data)
+		print ("recv data:" .. data)
+		uart.send(fd,"ok"..data)
 	end
-	uart.open(cb,"/dev/ttyS1")
+	fd = uart.open (cb, "/dev/ttyS1")
+	uart.set (fd, 115200, 0, 8, 1, 'N')
 end)
