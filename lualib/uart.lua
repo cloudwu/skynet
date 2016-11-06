924587bba32af631e0d7948492293f9629b938e7
mrChang0 <564415104@qq.com>
2016-11-06 14:17:39 +0800

    change some lua

--- lualib/uart.lua
+++ lualib/uart.lua
@@ -4,31 +4,12 @@ local skynet_core = require "skynet.core"
 local assert = assert
 
 local uart = {}	-- api
-local uart_pool = setmetatable ( -- store all socket object
-{},
-{ __gc = function (p)
-		for id, v in pairs (p) do
-			driver.close (id)
-			-- don't need clear v.buffer, because buffer pool will be free at the end
-			p[id] = nil
-		end
-	end
-}
-)
+local uart_pool = {}
 
-local socket_message = {}
+local uart_message = {}
 
-local function wakeup (s)
-	local co = s.co
-	if co then
-		s.co = nil
-		skynet.wakeup (co)
-	end
-end
-
--- read skynet_socket.h for these macro
 -- SKYNET_SOCKET_TYPE_DATA = 1
-socket_message[1] = function (id, size, data)
+uart_message[1] = function (id, size, data)
 	local s = uart_pool[id]
 	if s == nil or s.callback == nil then
 		skynet.error("uart: drop uart package from " .. id)
@@ -41,24 +22,22 @@ socket_message[1] = function (id, size, data)
 end
 
 -- SKYNET_SOCKET_TYPE_CONNECT = 2
-socket_message[2] = function (id, _, addr)
+uart_message[2] = function(id, _ , _)
 	local s = uart_pool[id]
 	if s == nil then
 		return
 	end
 	-- log remote addr
 	s.connected = true
-	wakeup (s)
 end
 
 -- SKYNET_SOCKET_TYPE_CLOSE = 3
-socket_message[3] = function (id)
+uart_message[3] = function (id)
 	local s = uart_pool[id]
 	if s == nil then
 		return
 	end
-	s.connected = false
-	wakeup (s)
+	uart_pool[id] = nil
 end
 
 skynet.register_protocol {
@@ -66,12 +45,12 @@ skynet.register_protocol {
 	id = skynet.PTYPE_SOCKET,	-- PTYPE_SOCKET = 6
 	unpack = driver.unpack,
 	dispatch = function (_, _, t, ...)
-		socket_message[t](...)
+		uart_message[t](...)
 	end
 }
 
 local function create_uart_object(id, cb)
-	assert(not uart_pool[id], "uart is not closed")
+	assert(not uart_pool[id], "uart is not close")
 	uart_pool[id] = {
 		id = id,
 		connected = false,
@@ -82,11 +61,45 @@ end
 
 function uart.open(callback,port)
 	local id = driver.open(port)
+	if id < 0 then
+		return id
+	end
 	create_uart_object(id, callback)
 	return id
 end
 
-uart.send = assert(driver.send)
-uart.set = driver.set
+function uart.close(id)
+	local s = uart_pool[id]
+	if s == nil then
+		return
+	end
+	uart_pool[id] = nil
+	driver.close(id)
+end
+
+function uart.send(id,data)
+	local s = uart_pool[id]
+	if s == nil then
+		skynet.error("uart is not open")
+		return
+	end
+	if s.connected == false then
+		skynet.error("uart should set first")
+		return
+	end
+	driver.send(id,data)
+end
+
+function uart.set(id, speed, flow_ctrl, databits, stopbits, parity)
+	local s = uart_pool[id]
+		if s == nil then
+		skynet.error("uart is not open")
+		return
+	end
+	local ok = driver.set(id, speed, flow_ctrl, databits, stopbits, parity)
+	if ok then
+		s.connected = true
+	end
+end
 
 return uart
--- test/testuart.lua
+++ test/testuart.lua
@@ -2,10 +2,15 @@ local skynet = require "skynet"
 local uart = require "uart"
 
 local fd
+
 skynet.start(function()
 	local cb = function(id,data)
-		print ("recv data:" .. data)
-		uart.send(fd,"ok"..data)
+		print("recv data:" .. data)
+		if string.sub(data,1,-2) == "quit" then
+			uart.close(fd)
+			skynet.exit()
+		end
+		uart.send(fd,"ok "..data)
 	end
 	fd = uart.open (cb, "/dev/ttyS1")
 	uart.set (fd, 115200, 0, 8, 1, 'N')
