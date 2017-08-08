556aa1f2594b438e30d9299c5bfcc4c5768b3a4e
hongl <hongling-0@qq.com>
2017-08-08 11:08:01 +0800

    Allow yeild in the skynet.init function of the service_cell service

--- service/service_cell.lua
+++ service/service_cell.lua
@@ -5,7 +5,42 @@ local init = {}
 
 function init.init(code, ...)
 	skynet.dispatch("lua", function() error("No dispatch function")	end)
-	skynet.start = function(f) f()	end
+
+	local init_func = {}
+	function skynet.init(f, name)
+		assert(type(f) == "function")
+		if init_func == nil then
+			f()
+		else
+			table.insert(init_func, f)
+			if name then
+				assert(type(name) == "string")
+				assert(init_func[name] == nil)
+				init_func[name] = f
+			end
+		end
+	end
+
+	local function init_all()
+		local funcs = init_func
+		init_func = nil
+		if funcs then
+			for _,f in ipairs(funcs) do
+				f()
+			end
+		end
+	end
+
+	local function ret(f, ...)
+		f()
+		return ...
+	end
+
+	function skynet.start(start_func)
+		init_all()
+		init_func = {}
+		return ret(init_all, start_func())
+	end
 	local mainfunc = assert(load(code, service_name))
 	mainfunc(...)
 	skynet.ret()
