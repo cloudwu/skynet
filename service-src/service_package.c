a6d7a5e97c6e8202e1c41e4e4d3da4f99d0cde8c
zsutxz <zsutxz@gmail.com>
2017-08-15 12:30:25 +0800

    修改：     service-src/service_package.c

--- service-src/service_package.c
+++ service-src/service_package.c
@@ -228,6 +228,8 @@ new_message(struct package *P, const uint8_t *msg, int sz) {
 			++msg;
 			--sz;
 		}
+		skynet_error(NULL,"P->header 0 :%d,1:%d",P->header[0],P->header[1]);
+
 		P->uncomplete.sz = P->header[0] * 256 + P->header[1];
 		P->uncomplete.msg = skynet_malloc(P->uncomplete.sz);
 		P->uncomplete_sz = P->uncomplete.sz;
