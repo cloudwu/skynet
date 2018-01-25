084aab4bcd18af738157e2a4c9d9621d544677cb
Cloud Wu <cloudwu@gmail.com>
2018-01-25 15:56:47 +0800

    dns server auto retire

--- test/testdns.lua
+++ test/testdns.lua
@@ -1,11 +1,20 @@
 local skynet = require "skynet"
 local dns = require "skynet.dns"
 
+local resolve_list = {
+	"github.com",
+	"stackoverflow.com",
+	"lua.com",
+}
+
 skynet.start(function()
 	print("nameserver:", dns.server())	-- set nameserver
 	-- you can specify the server like dns.server("8.8.4.4", 53)
-	local ip, ips = dns.resolve "github.com"
-	for k,v in ipairs(ips) do
-		print("github.com",v)
+	for _ , name in ipairs(resolve_list) do
+		local ip, ips = dns.resolve(name)
+		for k,v in ipairs(ips) do
+			print(name,v)
+		end
+		skynet.sleep(500)	-- sleep 5 sec
 	end
 end)
