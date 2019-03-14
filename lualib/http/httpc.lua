7746193b5fc4ff855ea7b2b11cc05cf9356563ba
zixun <lvzixun@gmail.com>
2019-03-14 10:36:02 +0800

    add https client and server support by bios

--- lualib/http/httpc.lua
+++ lualib/http/httpc.lua
@@ -8,9 +8,9 @@ local table = table
 
 local httpc = {}
 
-local function request(fd, method, host, url, recvheader, header, content)
-	local read = socket.readfunc(fd)
-	local write = socket.writefunc(fd)
+local function request(interface, method, host, url, recvheader, header, content)
+	local read = interface.read
+	local write = interface.write
 	local header_content = ""
 	if header then
 		if not header.host then
@@ -74,7 +74,7 @@ local function request(fd, method, host, url, recvheader, header, content)
 			end
 		else
 			-- no content-length, read all
-			body = body .. socket.readall(fd)
+			body = body .. interface.readall()
 		end
 	end
 
@@ -88,11 +88,60 @@ function httpc.dns(server,port)
 	dns.server(server,port)
 end
 
+
+local function check_protocol(host)
+	local protocol = host:match("^[Hh][Tt][Tt][Pp][Ss]?://")
+	if protocol then
+		host = string.gsub(host, "^"..protocol, "")
+		protocol = string.lower(protocol)
+		if protocol == "https://" then
+			return "https", host
+		elseif protocol == "http://" then
+			return "http", host
+		else
+			error(string.format("Invalid protocol: %s", protocol))
+		end
+	else
+		return "http", host
+	end
+end
+
+local SSLCTX_CLIENT = nil
+local function gen_interface(protocol, fd)
+	if protocol == "http" then
+		return {
+			init = nil,
+			close = nil,
+			read = socket.readfunc(fd),
+			write = socket.writefunc(fd),
+			readall = function ()
+				return socket.readall(fd)
+			end,
+		}
+	elseif protocol == "https" then
+		local tls = require "http.tlshelper"
+		SSLCTX_CLIENT = SSLCTX_CLIENT or tls.newctx()
+		local tls_ctx = tls.newtls("client", SSLCTX_CLIENT)
+		return {
+			init = tls.init_requestfunc(fd, tls_ctx),
+			close = tls.closefunc(tls_ctx),
+			read = tls.readfunc(fd, tls_ctx),
+			write = tls.writefunc(fd, tls_ctx),
+			readall = tls.readallfunc(fd, tls_ctx),
+		}
+	else
+		error(string.format("Invalid protocol: %s", protocol))
+	end
+end
+
+
 function httpc.request(method, host, url, recvheader, header, content)
+	local protocol
 	local timeout = httpc.timeout	-- get httpc.timeout before any blocked api
+	protocol, host = check_protocol(host)
 	local hostname, port = host:match"([^:]+):?(%d*)$"
 	if port == "" then
-		port = 80
+		port = protocol=="http" and 80 or protocol=="https" and 443
 	else
 		port = tonumber(port)
 	end
@@ -101,20 +150,31 @@ function httpc.request(method, host, url, recvheader, header, content)
 	end
 	local fd = socket.connect(hostname, port, timeout)
 	if not fd then
-		error(string.format("http connect error host:%s, port:%s, timeout:%s", hostname, port, timeout))
+		error(string.format("%s connect error host:%s, port:%s, timeout:%s", protocol, hostname, port, timeout))
 		return
 	end
+	-- print("protocol hostname port", protocol, hostname, port)
+	local interface = gen_interface(protocol, fd)
 	local finish
 	if timeout then
 		skynet.timeout(timeout, function()
 			if not finish then
 				socket.shutdown(fd)	-- shutdown the socket fd, need close later.
+				if interface.close then
+					interface.close()
+				end
 			end
 		end)
 	end
-	local ok , statuscode, body = pcall(request, fd,method, host, url, recvheader, header, content)
+	if interface.init then
+		interface.init()
+	end
+	local ok , statuscode, body = pcall(request, interface, method, host, url, recvheader, header, content)
 	finish = true
 	socket.close(fd)
+	if interface.close then
+		interface.close()
+	end
 	if ok then
 		return statuscode, body
 	else
