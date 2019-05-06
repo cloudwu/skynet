2c8d373236b57a6e761bf791cbdc0c88d2e21257
zixun <lvzixun@gmail.com>
2019-05-06 11:14:08 +0800

    fix use NOUSE_JEMALLOC

--- skynet-src/malloc_hook.c
+++ skynet-src/malloc_hook.c
@@ -257,6 +257,18 @@ mallctl_opt(const char* name, int* newval) {
 	return 0;
 }
 
+bool
+mallctl_bool(const char* name, bool* newval) {
+	skynet_error(NULL, "No jemalloc : mallctl_bool %s.", name);
+	return 0;
+}
+
+int
+mallctl_cmd(const char* name) {
+	skynet_error(NULL, "No jemalloc : mallctl_cmd %s.", name);
+	return 0;
+}
+
 #endif
 
 size_t
