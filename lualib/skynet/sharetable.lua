9e63baf75d762159c928fa52b8625f883c9ac3a0
cloudfreexiao <996442717qqcom@gmail.com>
2020-01-01 23:40:33 +0800

    修改 sharetable loadfile 以文件真实名做key值

--- lualib/skynet/sharetable.lua
+++ lualib/skynet/sharetable.lua
@@ -26,11 +26,44 @@ local function sharetable_service()
 		end
 	end
 
+    local function basename( path, ext ) --> name
+        local function rmsuffix( str, suffix )
+            local suffixlen = string.len( suffix )
+            local endsub = string.sub( str, -suffixlen )
+            if endsub == suffix then
+                local n = string.len( str ) - suffixlen
+                return string.sub( str, 1, n )
+            else
+                return str
+            end
+        end
+
+        local i = #path
+        local c = string.sub( path, i, i )
+        while i > 0 and c ~= "/" do
+            i = i - 1
+            c = string.sub( path, i, i )
+        end
+
+        local name = path
+        if i ~= 0 then
+            name = string.sub( path, i+1 )
+        end
+
+        if ext then
+            return rmsuffix( name, ext )
+        else
+            return name
+        end
+    end
+
+
 	function sharetable.loadfile(source, filename, ...)
-		close_matrix(files[filename])
-		local m = core.matrix("@" .. filename, ...)
-		files[filename] = m
-		skynet.ret()
+        local key = basename(filename, ".lua")
+        close_matrix(files[key])
+        local m = core.matrix("@" .. filename, ...)
+        files[key] = m
+        skynet.ret()
 	end
 
 	function sharetable.loadstring(source, filename, datasource, ...)
