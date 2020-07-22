f2b1bd73198ee68352d9acaebdcebef7e39bb3b2
fanlix <fanlix@gmail.com>
2020-07-22 19:26:39 +0800

    stat: add mem stat for jemalloc (#1218)
    1, add opts arg for mallctl,dumpinfo
    2, collect jemalloc.mem info
    3, add debug_console cmd: jmem
    
    Co-authored-by: fx <fx@f.x>

--- skynet-src/malloc_hook.c
+++ skynet-src/malloc_hook.c
@@ -127,8 +127,8 @@ static void malloc_oom(size_t size) {
 }
 
 void
-memory_info_dump(void) {
-	je_malloc_stats_print(0,0,0);
+memory_info_dump(const char* opts) {
+	je_malloc_stats_print(0,0, opts);
 }
 
 bool
@@ -241,7 +241,7 @@ skynet_posix_memalign(void **memptr, size_t alignment, size_t size) {
 #define raw_free free
 
 void
-memory_info_dump(void) {
+memory_info_dump(const char* opts) {
 	skynet_error(NULL, "No jemalloc");
 }
 
