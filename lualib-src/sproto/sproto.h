44485b195c28ddf15baec967b1e9e71b3df4b753
子熏 <lvzixun@gmail.com>
2020-08-03 22:54:04 +0800

    update sproto double array (#1227)
    Co-authored-by: zixun <lvzxiun@gmail.com>

--- lualib-src/sproto/sproto.h
+++ lualib-src/sproto/sproto.h
@@ -13,7 +13,7 @@ struct sproto_type;
 #define SPROTO_TINTEGER 0
 #define SPROTO_TBOOLEAN 1
 #define SPROTO_TSTRING 2
-#define SPROTO_DOUBLE 3
+#define SPROTO_TDOUBLE 3
 #define SPROTO_TSTRUCT 4
 
 // sub type of string (sproto_arg.extra)
