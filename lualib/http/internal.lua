a3242efbac7c12749f6ca58ab1b7d1a91fc7a236
王羽平.Cerberus <wangyuping.cerberus@snda.com>
2021-07-21 19:42:16 +0800

    'http请求C没有大写'

--- lualib/http/internal.lua
+++ lualib/http/internal.lua
@@ -160,11 +160,11 @@ function M.request(interface, method, host, url, recvheader, header, content)
 	end
 
 	if content then
-		local data = string.format("%s %s HTTP/1.1\r\n%scontent-length:%d\r\n\r\n", method, url, header_content, #content)
+		local data = string.format("%s %s HTTP/1.1\r\n%sContent-length:%d\r\n\r\n", method, url, header_content, #content)
 		write(data)
 		write(content)
 	else
-		local request_header = string.format("%s %s HTTP/1.1\r\n%scontent-length:0\r\n\r\n", method, url, header_content)
+		local request_header = string.format("%s %s HTTP/1.1\r\n%sContent-length:0\r\n\r\n", method, url, header_content)
 		write(request_header)
 	end
 
