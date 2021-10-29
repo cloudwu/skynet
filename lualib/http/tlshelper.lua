e35cad3053cd68e6bf64c19f93090f711f974e49
子熏 <lvzixun@gmail.com>
2021-10-29 13:55:54 +0800

    fix #1494 socket read阻塞问题 (#1495)
    * fix #1494 socket read阻塞问题
    
    * 优化readfunc
    
    Co-authored-by: zixun <lb151450@alibaba-inc.com>

--- lualib/http/tlshelper.lua
+++ lualib/http/tlshelper.lua
@@ -43,13 +43,16 @@ function tlshelper.closefunc(tls_ctx)
 end
 
 function tlshelper.readfunc(fd, tls_ctx)
-    local readfunc = socket.readfunc(fd)
+    local function readfunc()
+        readfunc = socket.readfunc(fd)
+        return ""
+    end
     local read_buff = ""
     return function (sz)
         if not sz then
             local s = ""
             if #read_buff == 0 then
-                local ds = readfunc(sz)
+                local ds = readfunc()
                 s = tls_ctx:read(ds)
             end
             s = read_buff .. s
