dd09a5a180311a8cbed5d9d813f3ecdc65b993dd
guping <brave_ping@icloud.com>
2022-06-21 17:04:32 +0800

    O(1) skynet.queue pop

--- lualib/skynet/queue.lua
+++ lualib/skynet/queue.lua
@@ -7,12 +7,20 @@ local table = table
 function skynet.queue()
 	local current_thread
 	local ref = 0
-	local thread_queue = {}
+	local thread_queue = { h = 1, t = 0 }
 
 	local function xpcall_ret(ok, ...)
 		ref = ref - 1
 		if ref == 0 then
-			current_thread = table.remove(thread_queue,1)
+			local h = thread_queue.h
+			current_thread = thread_queue[h]
+			thread_queue[h] = nil
+			thread_queue.h = h + 1
+			if thread_queue.h > thread_queue.t then
+				-- queue is empty
+				thread_queue.h = 1
+				thread_queue.t = 0
+			end
 			if current_thread then
 				skynet.wakeup(current_thread)
 			end
@@ -24,7 +32,8 @@ function skynet.queue()
 	return function(f, ...)
 		local thread = coroutine.running()
 		if current_thread and current_thread ~= thread then
-			table.insert(thread_queue, thread)
+			thread_queue.t = thread_queue.t + 1
+			thread_queue[thread_queue.t] = thread
 			skynet.wait()
 			assert(ref == 0)	-- current_thread == thread
 		end
