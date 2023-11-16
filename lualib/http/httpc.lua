ee47eedf8d3b624830447df4306c4c406a68a084
zhuilang <490240398@qq.com>
2023-11-16 10:13:45 +0800

    modify httpc.request
    这样可能更好点

--- lualib/http/httpc.lua
+++ lualib/http/httpc.lua
@@ -114,7 +114,7 @@ function httpc.request(method, hostname, url, recvheader, header, content)
 	close_interface(interface, fd)
 	if ok then
 		return statuscode, body
-	elseif statuscode == 200 then
+	elseif type(statuscode) == "number" then
 		error(body)
 	else
 		error(statuscode)
